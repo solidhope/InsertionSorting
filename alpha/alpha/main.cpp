57fa324a8a64b8e4ddf92aa012402f24afcf87b4
Justin P Hogue <hogue.justin@gmail.com>
2013-03-30 22:30:41 -0400

    Put the wrong code in, this is now Insertion Sort not Bubble Sort

--- alpha/alpha/main.cpp
+++ alpha/alpha/main.cpp
@@ -1,49 +1,44 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
-void bubbleSort(int data[], int n);
+void printArr(int array[], int size) {
+    
+    cout << endl;
+    cout<< "Sorting..... \n";
+    int j;
+    
+    for (j = 0; j < size; j++)
+        
+        cout << " " << array[j];
+    cout << endl;
+}
+
+void insertionSort(int arr[], int length) {
+    
+    int i, j , tmp;
+    
+    for (i = 1; i < length; i++) {
+        
+        j = i;
+        
+        while (j > 0 && arr[j - 1] > arr[j]) {
+            
+            tmp = arr[j];
+            arr[j] = arr[j - 1];
+            arr[j - 1] = tmp;
+            j--;
+        }
+        
+        printArr(arr, 10);
+    }
+}
 
 int main() {
     
-    cout << "Enter ten unsorted integers \n" << endl;
+    int array[10]= {5, 4, 3, 2, 1, 44, 68, 34, 8, 100};
     
-    int a[10];
-    for (int i = 0; i < 10; ++i) {
-        
-        cout << "[" << i << "] = ";
-        cin >> a[i];
-    }
+    insertionSort(array, 10);
+    cout << endl;
     
-    cout << "Sorted List = ";
-    bubbleSort(a, 10);
-}
-
-void bubbleSort(int data[], int n) {
-    
-    int j = 0;
-    bool nextEnd = true;
-    
-    while (nextEnd) {
-        
-        nextEnd = false;
-        j++;
-        
-        for (int i = 0; i < n - j; i++) {
-            
-            if (data[i] > data[i + 1]) {
-                
-                int temp = data[i + 1];
-                data[i + 1] = data[i];
-                data[i] = temp;
-                nextEnd = true;
-            }
-        }
-    }
-    
-    for (int i = 0; i < 10; i++)
-        
-        cout << data[i] << ", ";
-        cout << endl;
-    
+    return 0;
 }
